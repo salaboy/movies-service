d649fd8ae3f82459c6ecf2c4e86464e8682f509e
salaboy <salaboy@gmail.com>
2019-01-09 10:59:47 +0000

    autowiring WebTestClient

--- src/test/java/com/salaboy/movies/moviesservice/MoviesServiceApplicationTests.java
+++ src/test/java/com/salaboy/movies/moviesservice/MoviesServiceApplicationTests.java
@@ -2,18 +2,20 @@ package com.salaboy.movies.moviesservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MoviesServiceApplicationTests {
 
-    private WebTestClient testClient = WebTestClient
-            .bindToServer()
-            .baseUrl("http://localhost:8080")
-            .build();
+
+    @Autowired
+    private WebTestClient testClient;
+
+
     @Test
     public void contextLoads() {
     }
